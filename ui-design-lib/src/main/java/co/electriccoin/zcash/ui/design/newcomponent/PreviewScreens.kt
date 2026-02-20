3d3734046468aa2cf687ffbeb44cd13b24880991
NoOp <dominik@zodl.com>
2026-02-20 15:29:01 +0100

    New translations Added one preview in ES for testing

--- ui-design-lib/src/main/java/co/electriccoin/zcash/ui/design/newcomponent/PreviewScreens.kt
+++ ui-design-lib/src/main/java/co/electriccoin/zcash/ui/design/newcomponent/PreviewScreens.kt
@@ -6,7 +6,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import kotlin.annotation.AnnotationRetention.SOURCE
 
 @Preview(name = "1: Light preview", showBackground = true)
-@Preview(name = "2: Dark preview", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(name = "2: Dark preview", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES, locale = "es")
 @Retention(SOURCE)
 annotation class PreviewScreens
 
